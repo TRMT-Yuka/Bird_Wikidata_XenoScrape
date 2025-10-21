6622c97f817ba1f8113bef351344a1bbe0dbde63
TRMT-Yuka <teramoto@mil.doshisha.ac.jp>
2025-10-21 17:15:20 +0900

    delete some file

--- mymodule.py
+++ mymodule.py
@@ -22,17 +22,35 @@ def directory_to_dict(path):
         break
     return dir_dict
 
+def directory_to_tuple(path):
+    result = directory_to_dict(path)
+    tuples = set()
+    for bird_id,sound_ids in result.items():
+        for sound_id in sound_ids:
+            tuples.add((bird_id,sound_id))
+    return tuples
+
 # =================================================================================================
 import os
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
+import os
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+
+
+# path = "E:\\xeno_data\\"
+# path_trimmed = "E:\\trimmed_xeno_data\\"
+# path = ".\\xeno_data"
+# path_trimmed = ".\\trimmed_xeno_data"
 
 def process_audio(input_tuple):
     try:
         bird_id, sound_id = input_tuple
-        if os.path.exists("E:\\xeno_data\\"+bird_id+"\\"+sound_id) and not os.path.exists("E:\\trimmed_xeno_data\\"+bird_id+"\\"+sound_id):
-            sound = AudioSegment.from_mp3("E:\\xeno_data\\"+bird_id+"\\"+sound_id)
+        if os.path.exists("D:\\xeno_data\\"+bird_id+"\\"+sound_id) and not os.path.exists("D:\\trimmed_xeno_data\\"+bird_id+"\\"+sound_id):
+            sound = AudioSegment.from_mp3("D:\\xeno_data\\"+bird_id+"\\"+sound_id)
             chunks = split_on_silence(
                 sound,
                 min_silence_len=500,  # 0.5秒以上の無音を検出
@@ -46,10 +64,10 @@ def process_audio(input_tuple):
             if len(combined) > 30_000:
                 combined = combined[:30_000]
             
-            if not os.path.exists("E:\\trimmed_xeno_data\\"+bird_id):
-                os.makedirs("E:\\trimmed_xeno_data\\"+bird_id)
+            if not os.path.exists("D:\\trimmed_xeno_data\\"+bird_id):
+                os.makedirs("D:\\trimmed_xeno_data\\"+bird_id)
                 
-            combined.export("E:\\trimmed_xeno_data\\"+bird_id+"\\"+sound_id, format="mp3")
+            combined.export("D:\\trimmed_xeno_data\\"+bird_id+"\\"+sound_id, format="mp3")
 
     except:
         return input_tuple
